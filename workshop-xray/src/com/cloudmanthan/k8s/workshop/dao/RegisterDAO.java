0fc0adef414d937d778180c1abf4aee4ba4f7a54
amod.kadam@cloudmanthan.com <amod.kadam@cloudmanthan.com>
2019-04-11 19:32:51 +0530

    Added Pooled Connection

--- workshop-xray/src/com/cloudmanthan/k8s/workshop/dao/RegisterDAO.java
+++ workshop-xray/src/com/cloudmanthan/k8s/workshop/dao/RegisterDAO.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
 
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.entities.Subsegment;
@@ -15,6 +18,15 @@ import com.cloudmanthan.k8s.workshop.model.RegistrationInfo;
 public class RegisterDAO {
 
 	Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	String db_host = System.getenv("DB_HOST");
+	String db_port = System.getenv("DB_PORT");
+	String db_name = System.getenv("DB_NAME");
+	String db_user = System.getenv("DB_USER");
+	String db_password = System.getenv("DB_PASSWORD");
+	String driverClassName = "com.mysql.jdbc.Driver";
+	String conString = "jdbc:mysql://" + db_host + ":" + db_port + "/" + db_name ;
+
 
 	public boolean register(RegistrationInfo regInfo) {
 		
@@ -27,18 +39,13 @@ public class RegisterDAO {
 		AWSXRay.getCurrentSegment().putAnnotation("firstname", regInfo.getFirstName());
 		
 		try {
+			
 			Class.forName("com.mysql.jdbc.Driver");
-
-			String db_host = System.getenv("DB_HOST");
-			String db_port = System.getenv("DB_PORT");
-			String db_name = System.getenv("DB_NAME");
-			String db_user = System.getenv("DB_USER");
-			String db_password = System.getenv("DB_PASSWORD");
-
-			String conString = "jdbc:mysql://" + db_host + ":" + db_port + "/" + db_name ;
-
+			
 			logger.log(Level.INFO, conString);
-			Connection con = DriverManager.getConnection(conString, db_user, db_password);
+			//Connection con = DriverManager.getConnection(conString, db_user, db_password);
+			
+			Connection con = getPooledConnection();
 
 			PreparedStatement stmt = con.prepareStatement("insert into registration values(?,?)");
 			stmt.setString(1, regInfo.getFirstName());// 1 specifies the first parameter in the query
@@ -61,12 +68,26 @@ public class RegisterDAO {
 		logger.log(Level.INFO, "Sending Message");
 		
 		SendSNSNotification.sendMessage(regInfo);
-		logger.log(Level.INFO, "Message Sen");
+		logger.log(Level.INFO, "Message Sent");
 			
 		
 		
 		
 		return  success ;
 
+	}
+
+	private Connection getPooledConnection() throws SQLException {
+		
+		// Using JDBC Connection from Connection Pool provided by tomcat : DataSource
+        DataSource datasource = new DataSource();
+        datasource.setUrl(conString);
+        datasource.setUsername(db_user);
+        datasource.setPassword(db_password);
+        datasource.setDriverClassName(driverClassName);
+        datasource.setJdbcInterceptors("com.amazonaws.xray.sql.mysql.TracingInterceptor;");
+        return datasource.getConnection();
+
+		
 	}
 }
